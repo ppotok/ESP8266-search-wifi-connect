ad5f8dae5bea4e7d81a75b3afc41db5a993c689e
Potok <ppotok@users.noreply.github.com>
2015-08-02 15:53:48 +0200

    Added support for WEB update and mdns
    Navigate to http://esp8266-webupdate.local/upgrade  (or your name of lan) select .bin with new firmware and just press update.
    
    Currently is problem that I need to press hardware reset because it dont reset automatically.

--- main.ino
+++ main.ino
@@ -1,5 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <WiFiClient.h>
+
 
 const int ledPin =  2;
 int ledState = LOW; 
@@ -12,6 +15,9 @@ enum {
 };
 
 ESP8266WebServer server(80);
+/*for update*/
+const char* host = "esp8266-webupdate";
+const char* serverIndex = "<form method='POST' action='/update' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Update'></form>";
 
 const char* ssid = "Bubbles";  // Use this as the ssid as well
 // as the mDNS name
@@ -129,6 +135,7 @@ void launchWeb(int webservertype) {
 //---------------------------------------------------------------SETUP of Webpages
 void setupWebServerHandlers(int webservertype)
 {
+  MDNS.begin(host); //--------mdns
   if ( webservertype == ACCESS_POINT_WEBSERVER ) {
     server.on("/", handleDisplayAccessPoints);
     server.on("/setap", handleSetAccessPoint);
@@ -136,8 +143,45 @@ void setupWebServerHandlers(int webservertype)
   }
   else if (webservertype == APPLICATION_WEBSERVER) { // serving pages when ESP is connected
     server.on("/", handleRoot);
+    server.on("/upgrade", HTTP_GET, [](){
+      server.sendHeader("Connection", "close");
+      server.sendHeader("Access-Control-Allow-Origin", "*");
+      server.send(200, "text/html", serverIndex);
+    });
     server.on("/setap", handleAccessPointAlreadySet);
-
+    /*UPDATE PART*/
+server.onFileUpload([](){
+      if(server.uri() != "/update") return;
+      HTTPUpload& upload = server.upload();
+      if(upload.status == UPLOAD_FILE_START){
+        Serial.setDebugOutput(true);
+        WiFiUDP::stopAll();
+        Serial.printf("Update: %s\n", upload.filename.c_str());
+        uint32_t maxSketchSpace = (ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000;
+        if(!Update.begin(maxSketchSpace)){//start with max available size
+          Update.printError(Serial);
+        }
+      } else if(upload.status == UPLOAD_FILE_WRITE){
+        if(Update.write(upload.buf, upload.currentSize) != upload.currentSize){
+          Update.printError(Serial);
+        }
+      } else if(upload.status == UPLOAD_FILE_END){
+        if(Update.end(true)){ //true to set the size to the current progress
+          Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+        } else {
+          Update.printError(Serial);
+        }
+        Serial.setDebugOutput(false);
+      }
+      yield();
+    });
+    server.on("/update", HTTP_POST, [](){
+      server.sendHeader("Connection", "close");
+      server.sendHeader("Access-Control-Allow-Origin", "*");
+      server.send(200, "text/plain", (Update.hasError())?"FAIL":"OK");
+      ESP.restart();
+    });
+    /*UPDATE PART*/
 /*server.on ( "/time", []() { server.send ( 200, "text/plain", getactualtime() );  } );
   server.on ( "/date", []() { server.send ( 200, "text/plain", getactualdate() );  } );*/
     
